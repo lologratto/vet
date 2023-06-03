35701aaaf44d7f5396701854c858ec5296f00429
lorenzo <lorenzograttone@gmail.com>
2023-06-02 21:25:46 -0300

    avance en el diseño y login

---
+++ php/cabecera.php
@@ -0,0 +1,27 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+  <img src="templete/assets/img/vet_logo.png" width="100px" alt="">
+      <h1 class="logo me-auto"><a href="index.html">Veterinaria</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Acerca de Nosotros</a></li>
+          <li><a class="nav-link scrollto" href="#services">Servicios</a></li>
+          
+       
+          
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+          </button>
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
