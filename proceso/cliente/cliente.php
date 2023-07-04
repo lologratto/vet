7a4f42c3b01440cc2b80dd195e59b2b2668ce10d
lorenzo <lorenzograttone@gmail.com>
2023-07-04 19:06:37 -0300

    doc actualizado

--- proceso/cliente/cliente.php
+++ proceso/cliente/cliente.php
@@ -3,7 +3,7 @@ require "../../php/cabecera2.php"
 ?>
 
 <title>GraVet</title>
-<link href="templete/assets/img/vet_logo_chico.png" rel="icon">
+<link href="../../templete/assets/img/vet_logo_chico.png" rel="icon">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <link href="../../templete/assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="../../templete/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
